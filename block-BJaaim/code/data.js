a8b3b9e122e365bae4b925f9f42999f1f646a0ed
atul1104 <waghamareatul2@gmail.com>
2022-02-08 23:12:12 +0530

    assignment

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,37 @@
+const quizCollection = [
+  {
+    title: "What temperature does water boil at?",
+    options: [
+      "50 degree celcius",
+      "25 degree celcius",
+      "100 degree celcius",
+      "150 degree celcius",
+    ],
+    answerIndex: 2,
+  },
+  {
+    title: "Who wrote Julius Caesar?",
+    options: [
+      "Whole Soyenka",
+      "Willium Shakespeare",
+      "Ngozi Chimanda",
+      "Dan Brown",
+    ],
+    answerIndex: 1,
+  },
+  {
+    title: "What is capital of India?",
+    options: ["Delhi", "Amaravati", "Mumbai", "Pune"],
+    answerIndex: 0,
+  },
+  {
+    title: "Who is prime minister of India?",
+    options: ["J. Nehru", "N Modi", "V P Singh", "Manmohan Singh"],
+    answerIndex: 1,
+  },
+  {
+    title: "Who is the father of nation?",
+    options: ["M K Gandhi", "J Nehru", "Vallabh bhai patel", "N Modi"],
+    answerIndex: 0,
+  },
+];
